2f54dfdd875a90d9631d2187e3ea181f657ac90f
Mansi Pawar <mansipawar1801@gmail.com>
2023-10-17 23:03:52 +0530

    First Commit

---
+++ bus_booking/home.php
@@ -0,0 +1,11 @@
+ <section id="bg-bus" class="d-flex align-items-center">
+    <div class="container">
+    <center><button class="btn btn-primary" type="button" id="book_now">Book Now</button></center>
+    </div>
+  </section>
+  <script>
+  	$('#book_now').click(function(){
+      uni_modal('Find Schedule','book_filter.php')
+  })
+  </script>
+
